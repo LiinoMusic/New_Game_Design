9ab78d943ac57b1a5e39b5af06a723d60784e501
LiinoMusic <feierliamnoel20@gmail.com>
2026-04-09 11:11:54 +0200

    Refactoring: Attacke wurde verbessert und so programmiert dass ich in jede richtung die attacke auführen kann und attack über das moving priorisiert wird

--- main1.py
+++ main1.py
@@ -38,13 +38,16 @@ Right = FRAME_HEIGHT * 2
 Back = FRAME_HEIGHT * 3
 
 #attack animation
+Sprites_attack_all = {}
 attack_sheet = pygame.image.load('Swordsman_lvl3_attack_with_shadow.png').convert_alpha()
-Sprites_attack = []
-for i in range(8):
-    Sprite_attack = attack_sheet.subsurface(FRAME_WIDTH * i, n, FRAME_WIDTH, FRAME_WIDTH)
-    Sprite_attack = pygame.transform.scale(Sprite_attack, (player_width, player_height))
-    Sprites_attack.append(Sprite_attack)
-
+for direction in [Front, Left, Right, Back]:
+    frames = []
+    for i in range(8):
+        frame = attack_sheet.subsurface(FRAME_WIDTH * i, direction, FRAME_WIDTH, FRAME_WIDTH)
+        frame = pygame.transform.scale(frame, (player_width, player_height))
+        frames.append(frame)
+    Sprites_attack_all[direction] = frames
+    
 #Standing Sprite
 Sprites_Stand_all = {}
 Spritesheet = pygame.image.load("Swordsman_lvl3_Idle_with_shadow.png").convert_alpha()
@@ -76,40 +79,46 @@ animation_timer = 0
 #Game Loop
 while game_is_running:
     moving = False
-    attack = False
     standing = False
     SCREEN.fill(backgroundcolor)
     clock.tick(FPS)
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_is_running = False
-        keys = pygame.key.get_pressed()
         #Fullscreen 
         if keys[pygame.K_F11]:
             pygame.display.toggle_fullscreen()
+        #trigger for attack
         elif (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.display.toggle_fullscreen()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                    attack = True
+                    animation_index_attack = 0
 
     #Player movement
     if keys[pygame.K_w]:
         player.y -= player_speed
-        moving = True
-        n = Back
+        if not attack:
+            moving = True
+            n = Back
     if keys[pygame.K_s]:
         player.y += player_speed
-        moving = True
-        n = Front
+        if not attack:
+            moving = True
+            n = Front
     if keys[pygame.K_a]:
         player.x -= player_speed
-        moving = True
-        n = Left
+        if not attack:
+            moving = True
+            n = Left
     if keys[pygame.K_d]:
         player.x += player_speed
-        moving = True
-        n = Right
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-                attack = True
+        if not attack:
+            moving = True
+            n = Right
 
     #if player went out of map
     if player.y >= HEIGHT - player_height//1.3:
@@ -125,10 +134,10 @@ while game_is_running:
 
     #Screen
     SCREEN.blit(background, (0, 0))
-    if moving == True:
-        SCREEN.blit(Sprites_walking_all[n][animation_index_walking], player.topleft)
     if attack == True:
-        SCREEN.blit(Sprites_attack[animation_index_attack], player.topleft)
+        SCREEN.blit(Sprites_attack_all[n][animation_index_attack], player.topleft)
+    elif moving == True:
+        SCREEN.blit(Sprites_walking_all[n][animation_index_walking], player.topleft)
     elif moving == False:
         SCREEN.blit(Sprites_Stand_all[n][animation_index_stand], player.topleft)
     pygame.display.flip()
@@ -136,7 +145,6 @@ while game_is_running:
     if animation_timer == 10:
         animation_index_stand += 1
         animation_index_walking += 1
-        animation_index_attack += 1
         if n == Back:
             if animation_index_stand > 3:
                 animation_index_stand = 0
@@ -144,8 +152,11 @@ while game_is_running:
             animation_index_stand = 0
         if animation_index_walking > 5:
             animation_index_walking = 0
-        if animation_index_attack > 7:
-            animation_index_attack = 0
+        if attack:
+            animation_index_attack += 1
+            if animation_index_attack > 7:
+                animation_index_attack = 0
+                attack = False
         animation_timer = 0
     animation_timer += 1
 
